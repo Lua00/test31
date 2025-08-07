2145ae72275bca4a8d2f781e5f029c1734f34a69
Lua00 <62857758+Lua00@users.noreply.github.com>
2025-08-08 02:02:54 +0300

    Delete site directory

--- site/public/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', function(e){
-  var close = e.target.closest('[data-close]');
-  if (close){
-    var target = close.closest('.flash');
-    if (target) target.remove();
-  }
-});
--- site/scripts/init_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/db.php';
-initSchema();
-echo "DB initialized at " . realpath(__DIR__ . '/../data/app.sqlite') . PHP_EOL;
